c35d594bbff84493525038a7936dbdcb4b5c0edd
YuriSamp <yurisamp123@gmail.com>
2023-02-08 01:36:56 -0300

    sketching the sidebar

--- components/Sidebar/index.tsx
+++ components/Sidebar/index.tsx
@@ -1,8 +1,28 @@
+import { BsSearch, BsGear, BsTrash } from 'react-icons/bs'
 
 export const Sidebar = () => {
   return (
-    <div>
-      Ola mundo
-    </div>
+    <aside className='w-80 min-h-screen flex flex-col items-center'>
+
+      <h1 className='pt-24'>FYESTA</h1>
+
+      <p className='py-12'>Menu</p>
+
+      <section className='flex flex-col gap-4'>
+        <div className='flex gap-3'>
+          <BsSearch />
+          <p>Search</p>
+        </div>
+        <div className='flex gap-3'>
+          <BsGear />
+          <p>Settings</p>
+        </div>
+        <div className='flex gap-3'>
+          <BsTrash />
+          <p>Trash</p>
+        </div>
+      </section>
+    </aside>
+
   )
 }
