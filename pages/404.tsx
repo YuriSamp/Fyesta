ba06ebef4d7d2146910b72a4ab884f9fdf227bc0
YuriSamp <yurisamp123@gmail.com>
2023-02-20 00:50:30 -0300

    fixing deploy bug

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,18 +1,22 @@
 import { Navbar } from '@ui/layout/navbar'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
+import { useSignOut } from 'react-firebase-hooks/auth'
 import { } from 'react-icons'
 import { AiOutlineArrowLeft } from 'react-icons/ai'
+import { auth } from 'server/Firebase/ClientApp'
 
 export default function NotFound() {
 
   const router = useRouter()
   const page = router.pathname
+  const [signOut] = useSignOut(auth);
 
   return (
     <section>
       <Navbar
         Page={page}
+        LogOut={signOut}
       />
       <section className='flex flex-col mx-12 my-12 '>
         <div className='flex flex-col justify-center items-center min-h-screen'>
