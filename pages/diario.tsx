f54e9d3f21af8301de62136421d5e1b5f3a79b3d
YuriSamp <yurisamp123@gmail.com>
2023-02-21 20:37:11 -0300

    adding diary page

--- pages/diario.tsx
+++ pages/diario.tsx
@@ -1,12 +1,24 @@
+import dynamic from 'next/dynamic';
+import { useState } from 'react';
+
+const PaginaDiario = dynamic(() => import('../components/diario/page'), {
+  ssr: false
+})
+
 
 export default function Diario() {
+
+  const [IsDiarioOpen, setDiarioOpen] = useState(false)
+
   return (
     <>
       <div className='border-b-2 border-gray-800'>
         <p className='text-3xl mb-2'>Fevereiro 2023</p>
       </div>
       <div className='flex gap-[22px] pt-4'>
-        <div className='w-60 h-52 border-2 border-[#383838] flex justify-center items-center cursor-pointer hover:bg-[#383838]'>
+        <div className='w-60 h-52 border-2 border-[#383838] flex justify-center items-center cursor-pointer hover:bg-[#383838]'
+          onClick={() => setDiarioOpen(true)}
+        >
           <p> + Entrada</p>
         </div>
         <div className='w-60 h-52 border-2 border-[#383838] flex px-2 py-2 cursor-pointer relative'>
@@ -95,6 +107,10 @@ export default function Diario() {
         </div>
 
       </div>
+      <PaginaDiario
+        State={IsDiarioOpen}
+        SetState={setDiarioOpen}
+      />
     </>
   )
 }
