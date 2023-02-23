4b19fdc437a122e08ec86ab09922332253b70ca4
YuriSamp <yurisamp123@gmail.com>
2023-02-23 00:25:16 -0300

    creating emotion and goals pages content

--- pages/metas.tsx
+++ pages/metas.tsx
@@ -1,8 +1,28 @@
 
 export default function Metas() {
   return (
-    <div className='flex justify-center items-center'>
-      <p>Em construção</p>
+    <div className='flex justify-center items-center gap-12'>
+      <div className='flex flex-col'>
+        <h1>Pretendo Realizar</h1>
+        <div className='pt-4 flex flex-col gap-4'>
+
+        </div>
+      </div>
+
+      <div className='flex flex-col'>
+        <h1>Em andamento</h1>
+        <div className='pt-4 flex flex-col gap-4'>
+
+        </div>
+      </div>
+
+
+      <div className='flex flex-col'>
+        <h1>Realizada</h1>
+        <div className='pt-4 flex flex-col gap-4'>
+
+        </div>
+      </div>
     </div>
   )
 }
