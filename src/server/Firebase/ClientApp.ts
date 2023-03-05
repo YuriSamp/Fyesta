dbb0e3efeaf93232682e84db7d1968d709482792
YuriSamp <yurisamp123@gmail.com>
2023-03-05 01:28:31 -0300

    env changes

--- src/server/Firebase/ClientApp.ts
+++ src/server/Firebase/ClientApp.ts
@@ -1,13 +1,14 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
+import { env } from 'src/env.mjs';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDvXVnNA7Wwwul0zxF4sMS4FTMKOfPtIpk',
-  authDomain: 'fyesta-trello.firebaseapp.com',
-  projectId: 'fyesta-trello',
-  storageBucket: 'fyesta-trello.appspot.com',
-  messagingSenderId: '441776517983',
-  appId: '1:441776517983:web:85ae2234b24d200f773cea',
+  apiKey: env.NEXT_PUBLIC_FIREBASE_APIKEY,
+  authDomain: env.NEXT_PUBLIC_AUTHDOMAIN,
+  projectId: env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: env.NEXT_PUBLIC_STORAGEBUCKET,
+  messagingSenderId: env.NEXT_PUBLIC_MESSAGINGSENDERID,
+  appId: env.NEXT_PUBLIC_APPID,
 };
 
 // Initialize Firebase
